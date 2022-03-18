ef50a7fac6780aeab76c77567037c54837e95536
Daria Bak <daria.bak22@gmail.com>
2022-03-18 00:59:29 +0000

    Added functionality to upload picture

--- screens/Profile.js
+++ screens/Profile.js
@@ -1,15 +1,11 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet, Image, ScrollView, Button} from "react-native";
+import { View, Text, StyleSheet, Image, ScrollView, Button, PermissionsAndroid} from "react-native";
 import AsyncStorage from '@react-native-async-storage/async-storage';
-
 import Constants from 'expo-constants';
 import * as FileSystem from 'expo-file-system';
 import { loginContext } from "../loginContext";
-
-
-
-
-
+import {launchCamera, launchImageLibrary} from 'react-native-image-picker'; 
+import * as ImagePicker from 'expo-image-picker';
 
 class Profile extends Component {
 static contextType = loginContext;
@@ -42,7 +38,8 @@ static contextType = loginContext;
             console.log(error);
         })
     }
-    getUserPhoto = () => {
+    getUserPhoto = async() => {
+        await FileSystem.deleteAsync(FileSystem.documentDirectory + 'profilePicture')
         FileSystem.downloadAsync(
             'http://10.0.2.2:3333/api/1.0.0/user/' + this.context.id + '/photo',
             FileSystem.documentDirectory + 'profilePicture',
@@ -59,7 +56,59 @@ static contextType = loginContext;
             this.getUserDetails();
         
     }
-    
+
+    requestCameraPermission = async () => {
+        try {
+          const granted = await PermissionsAndroid.request(
+            PermissionsAndroid.PERMISSIONS.CAMERA,
+            {
+              title: "App Camera Permission",
+              message:"App needs access to your camera ",
+              buttonNeutral: "Ask Me Later",
+              buttonNegative: "Cancel",
+              buttonPositive: "OK"
+            }
+          );
+          if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+            console.log("Camera permission given");
+          } else {
+            console.log("Camera permission denied");
+          }
+        } catch (err) {
+          console.warn(err);
+        }
+      };
+
+
+    uploadProfilePicture = async () => {
+        await this.requestCameraPermission();
+        let result = await ImagePicker.launchCameraAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            quality: 1,
+            base64: true
+          });
+          const image = await fetch(result.uri);
+          const blob = await image.blob();
+          
+          await this.uploadImage(blob);
+    }
+
+    uploadImage = async (blob) => {
+        fetch('http://10.0.2.2:3333/api/1.0.0/user/' + this.context.id + '/photo' , {
+        method: 'POST', 
+        body: blob,   
+        headers: {
+                'Content-Type': 'image/jpeg',
+                'X-Authorization': this.context.token
+            }
+        })
+        .then((response) => response.text())
+        .then((json) => console.log(json))
+        .catch((error) => {
+            console.log(error);
+        })
+    }
+
     render() {
         const url = 'http://10.0.2.2:3333/api/1.0.0/user/' + this.context.id + '/photo';
         if(this.state.isLoading){
@@ -84,6 +133,7 @@ static contextType = loginContext;
                     <Text style={styles.userName}>First name: {this.state.user_data.first_name} </Text>
                     <Text>Last name: {this.state.user_data.last_name}</Text>
                     <Text>Friends: {this.state.user_data.friend_count}</Text>
+                    <Button title='Change profile photo' onPress={() => this.uploadProfilePicture()}/>
                     <Button title='Edit profile' onPress={() => this.props.navigation.navigate('EditProfile')}/>
                     <Button title='Friends' onPress={() => this.props.navigation.navigate('Friends')}/>
                     
