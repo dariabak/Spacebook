07382cc6446eb9a02ee6154d3242edc073a18496
Daria Bak <daria.bak22@gmail.com>
2022-03-18 16:05:57 +0000

    Added functionality to reload friend requests after accepting or declining one

--- components/FriendRequest.js
+++ components/FriendRequest.js
@@ -1,12 +1,12 @@
 import { StyleSheet, Text, View, Button, TextInput, ColorPropType, Image, TouchableOpacity } from 'react-native';
 import React, { Component } from 'react';
-import { loginContext } from '../loginContext';
+import { LoginContext } from '../LoginContext';
 import * as FileSystem from 'expo-file-system';
 import uuid from 'react-native-uuid';
 
 class FriendRequest extends Component {
-    static contextType = loginContext;
-    constructor(props){
+    static contextType = LoginContext;
+    constructor(props) {
         super(props);
         this.state = {
             user_photo: null
@@ -16,87 +16,87 @@ class FriendRequest extends Component {
         this.getUserPhoto();
     }
     acceptFriendRequest = () => {
-        fetch('http://10.0.2.2:3333/api/1.0.0/friendrequests/' + this.props.request.user_id , {
-        method: 'POST',    
-        headers: {
+        fetch('http://10.0.2.2:3333/api/1.0.0/friendrequests/' + this.props.request.user_id, {
+            method: 'POST',
+            headers: {
                 'X-Authorization': this.context.token
             }
         })
-        .then((response) => {
-            if(response.status == 200) {
-                this.props.update;
-            } 
-        })
-        .catch((error) => {
-            console.log(error);
-        })
+            .then((response) => {
+                if (response.status == 200) {
+                    this.props.update();
+                }
+            })
+            .catch((error) => {
+                console.log(error);
+            })
     }
 
     declineFriendRequest = () => {
-        fetch('http://10.0.2.2:3333/api/1.0.0/friendrequests/' + this.props.request.user_id , {
-        method: 'DELETE',    
-        headers: {
+        fetch('http://10.0.2.2:3333/api/1.0.0/friendrequests/' + this.props.request.user_id, {
+            method: 'DELETE',
+            headers: {
                 'X-Authorization': this.context.token
             }
         })
-        .then((response) => {
-            if(response.status == 200) {
-                this.props.update.updateRequests();
-            } 
-        })
-        .catch((error) => {
-            console.log(error);
-        })
+            .then((response) => {
+                if (response.status == 200) {
+                    this.props.update();
+                }
+            })
+            .catch((error) => {
+                console.log(error);
+            })
     }
 
     getUserPhoto = () => {
         FileSystem.downloadAsync(
             'http://10.0.2.2:3333/api/1.0.0/user/' + this.props.request.user_id + '/photo',
             FileSystem.cacheDirectory + uuid.v4(),
-            {headers: {"X-Authorization": this.context.token}})
-            .then(({uri}) => {
-                this.setState({user_photo: uri})
+            { headers: { "X-Authorization": this.context.token } })
+            .then(({ uri }) => {
+                this.setState({ user_photo: uri })
             })
     }
 
     render() {
         return (
-            
-            <View style={{flexDirection: 'row', padding:5, width: '100%'}}>
-            <Image 
+
+            <View style={{ flexDirection: 'row', padding: 5, width: '100%' }}>
+                <Image
                     source={{
                         uri: this.state.user_photo,
-                        }}
-                        style={{
+                    }}
+                    style={{
                         width: 50,
                         height: 50,
                         borderWidth: 5,
-                        borderRadius: 25, 
+                        borderRadius: 25,
                         margin: 5
-                        }}/>
-            <Text style={{marginTop: 12, fontSize: 18, marginLeft: 5, marginRight: 15}}> {this.props.request.first_name} {this.props.request.last_name}</Text>
-           
-                <View style={{position: 'absolute', right: 5, marginTop: 12, flexDirection: 'row-reverse'}}>
+                    }} />
+                <Text style={{ marginTop: 12, fontSize: 18, marginLeft: 5, marginRight: 15 }}> {this.props.request.first_name} {this.props.request.last_name}</Text>
+
+                <View style={{ position: 'absolute', right: 5, marginTop: 12, flexDirection: 'row-reverse' }}>
                     <TouchableOpacity style={styles.button} onPress={() => this.declineFriendRequest()}>
-                        <Text style={{color: '#ffffff', fontSize: 16, padding: 5}}>Decline</Text>
+                        <Text style={{ color: '#ffffff', fontSize: 16, padding: 5 }}>Decline</Text>
                     </TouchableOpacity>
                     <TouchableOpacity style={styles.button} onPress={() => this.acceptFriendRequest()}>
-                        <Text style={{color: '#ffffff', fontSize: 16, padding: 5}}>Accept</Text>
+                        <Text style={{ color: '#ffffff', fontSize: 16, padding: 5 }}>Accept</Text>
                     </TouchableOpacity>
-                    
+
                 </View>
             </View>
-          
+
         );
     }
 }
 
-const styles = StyleSheet.create( {
+const styles = StyleSheet.create({
     button: {
-        backgroundColor:'#B22222', 
-        margin: 5, 
-        alignContent: 'center', 
-        justifyContent: 'center', 
+        backgroundColor: '#B22222',
+        margin: 5,
+        alignContent: 'center',
+        justifyContent: 'center',
         borderRadius: 5
     }
 });
--- screens/FriendRequests.js
+++ screens/FriendRequests.js
@@ -5,25 +5,26 @@ import FriendRequest from '../components/FriendRequest';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import UserItem from '../components/UserItem';
-import { loginContext } from '../loginContext';
+import { LoginContext } from '../LoginContext';
 import { styles } from '../styles/style';
 
 
 
 class FriendRequests extends Component {
-    static contextType = loginContext;
+    static contextType = LoginContext;
     constructor(props) {
         super(props);
         this.state = {
             isLoading: true,
             friendRequests: [],
             search_data: [],
-            search: ''
+            search: '',
+            getFriendRequests: this.getFriendRequests
         };
     }
 
-    
-    getFriendsRequests = () => {
+
+    getFriendRequests = () => {
         fetch('http://10.0.2.2:3333/api/1.0.0/friendrequests', {
             method: 'GET',
             headers: {
@@ -31,61 +32,58 @@ class FriendRequests extends Component {
                 'X-Authorization': this.context.token
             }
         })
-        .then((response) => {
-            return response.json()
-        })
-        .then((json) => {
-            console.log(json);
-            this.setState({friendRequests: json, isLoading: false});
-        })
-        .catch((error) => {
-            console.log(error);
-        })
+            .then((response) => {
+                return response.json()
+            })
+            .then((json) => {
+                console.log(json);
+                this.setState({ friendRequests: json, isLoading: false });
+            })
+            .catch((error) => {
+                console.log(error);
+            })
 
     }
-      
+
     componentDidMount() {
-       
-            this.getFriendsRequests();
-            this.props.navigation.setOptions({
-                headerRight: () => (
-                    <TouchableOpacity style={styles.buttonContainer} onPress={() => this.props.navigation.navigate('FriendSearch')}>
-                        <Text>Search Friends</Text>
-                    </TouchableOpacity>
-                  )
-            });
+
+        this.getFriendRequests();
+        this.props.navigation.setOptions({
+            headerRight: () => (
+                <TouchableOpacity style={styles.buttonContainer} onPress={() => this.props.navigation.navigate('FriendSearch')}>
+                    <Text>Search Friends</Text>
+                </TouchableOpacity>
+            )
+        });
     }
     render() {
-        if(this.state.isLoading) {
-            return(
+        if (this.state.isLoading) {
+            return (
                 <View>
                     <ActivityIndicator animating size='large' />
                 </View>
             );
         } else {
-        return (
-            <View style={{ flex: 1}}>
-           
-
-            {this.state.friendRequests.map(request => {
-               return( 
-               <View>     
-                    <FriendRequest key={request.user_id} request={request} update={{updateRequests: () => this.getFriendsRequests()}}/>
-                    <View
-                        style={{
-                        height: 1,
-                        width: '86%',
-                        backgroundColor: '#CED0CE',
-                        marginLeft: '14%',
-                        }}
-                    />
-                    </View>);
-            }
-                 )}
-            
-            </View>
-        );
+            return (
+                <View style={{ flex: 1 }}>
+                    {this.state.friendRequests.map(request => {
+                        return (
+                            <View>
+                                <FriendRequest key={request.user_id} request={request} update={this.state.getFriendRequests} />
+                                <View
+                                    style={{
+                                        height: 1,
+                                        width: '86%',
+                                        backgroundColor: '#CED0CE',
+                                        marginLeft: '14%',
+                                    }}
+                                />
+                            </View>);
+                    }
+                    )}
+                </View>
+            );
+        }
     }
-}
 }
 export default FriendRequests;
